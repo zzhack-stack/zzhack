632befa7af5df843687b35ba25e4d0e0e67453c6
mist.zzh <mist.zzh@gmail.com>
2022-06-07 21:12:43 +0800

    [Feat] publish genesis post

--- services/src/posts.rs
+++ services/src/posts.rs
@@ -6,22 +6,10 @@ pub struct PostFile {
     pub modified_time: u128
 }
 
-pub static POSTS: [PostFile; 4] = [
+pub static POSTS: [PostFile; 1] = [
     PostFile {
-    content: include_str!("../../posts/bar.md"),
-    modified_time: 1654101374572
-},
-PostFile {
-    content: include_str!("../../posts/foo.md"),
-    modified_time: 1654153316337
-},
-PostFile {
-    content: include_str!("../../posts/asd.md"),
-    modified_time: 1654489203129
-},
-PostFile {
-    content: include_str!("../../posts/test.md"),
-    modified_time: 1654489296116
+    content: include_str!("../../posts/build_blog.md"),
+    modified_time: 1654607237289
 },
 
 ];
