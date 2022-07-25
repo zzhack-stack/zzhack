edcc2d83b5f64b42215f4ad49b993201eeffa415
Mist <mist.zzh@gmail.com>
2022-07-25 21:45:04 +0800

    [Fix] compile error

--- ui/src/common/header/pages.rs
+++ ui/src/common/header/pages.rs
@@ -5,21 +5,4 @@ pub struct Page {
     pub name: &'static str,
 }
 
-pub const PAGES: [Page; 4] = [
-    Page {
-        route: RootRoutes::Home,
-        name: "Posts",
-    },
-    Page {
-        route: RootRoutes::Projects,
-        name: "Projects",
-    },
-    Page {
-        route: RootRoutes::About,
-        name: "About",
-    },
-    Page {
-        route: RootRoutes::Links,
-        name: "Links",
-    },
-];
+pub const PAGES: [Page; 0] = [];
