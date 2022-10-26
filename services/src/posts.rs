40e32f1bdb62570695f7fa1c7975d1e19813ad66
mist.zzh <mist.zzh@gmail.com>
2022-10-26 11:47:26 +0800

    [Feat] add mlog & update ci script

--- services/src/posts.rs
+++ services/src/posts.rs
@@ -7,8 +7,13 @@ pub struct PostFile {
     pub filename: &'static str
 }
 
-pub static POSTS: [PostFile; 2] = [
+pub static POSTS: [PostFile; 3] = [
     PostFile {
+    content: include_str!("../../posts/mlog_2022-10-26.md"),
+    modified_time: 1666755490770,
+    filename: "mlog_2022-10-26"
+},
+PostFile {
     content: include_str!("../../posts/build_blog.md"),
     modified_time: 1654609915763,
     filename: "build_blog"
