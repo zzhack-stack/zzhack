8c361e7be25eb10545cd98dffe60e04c35d61a38
Mist <mist.zzh@gmail.com>
2024-07-16 21:43:32 +0800

    [Remove] trim content field from post

--- shared/src/post.rs
+++ shared/src/post.rs
@@ -12,7 +12,6 @@ pub struct PaginationPostsRes {
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Post {
     pub path: String,
-    pub content: String,
     pub spoiler: String,
     pub title: String,
     pub created_at: String,
