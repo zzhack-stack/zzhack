bf14dc90fc41dcfdf59e1a706087f78ac49b98e6
Mist <mist.zzh@gmail.com>
2024-07-16 21:43:32 +0800

    [Remove] trim content field from post

--- api/src/dao/post.rs
+++ api/src/dao/post.rs
@@ -19,16 +19,17 @@ pub fn get_posts_by_page(
     page_limit: usize,
 ) -> rusqlite::Result<Vec<rusqlite::Result<Post>>> {
     execute(move |conn| {
-        let mut statement = conn.prepare("SELECT * FROM posts LIMIT ?1 OFFSET ?2")?;
+        let mut statement = conn.prepare(
+            "SELECT path, title, spoiler, created_at, updated_at FROM posts LIMIT ?1 OFFSET ?2",
+        )?;
         let posts_rows = statement
             .query_map(params!(page_limit, page), |row| {
                 Ok(Post {
-                    path: row.get(1)?,
-                    content: row.get(2)?,
-                    title: row.get(3)?,
-                    spoiler: row.get(4)?,
-                    created_at: row.get(5)?,
-                    updated_at: row.get(6)?,
+                    path: row.get(0)?,
+                    title: row.get(1)?,
+                    spoiler: row.get(2)?,
+                    created_at: row.get(3)?,
+                    updated_at: row.get(4)?,
                 })
             })?
             .collect::<Vec<rusqlite::Result<Post>>>();
