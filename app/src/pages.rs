fb76ff984b2836ddb290dccc8a207776a5842279
Mist <mist.zzh@gmail.com>
2024-07-17 17:16:24 +0800

    [Feat] add placeholder of links page and add corresponding todo item

--- app/src/pages.rs
+++ app/src/pages.rs
@@ -1,4 +1,5 @@
 pub mod home;
+pub mod links;
 pub mod not_found;
 pub mod post;
 pub mod posts;
---
+++ app/src/pages/links.rs
@@ -0,0 +1,6 @@
+use yew::prelude::*;
+
+#[function_component]
+pub fn Links() -> HtmlResult {
+    Ok(html! {"Links page"})
+}
